6dd7e6937be64dbc1f654905f8bb5ffce6736515
ChandiniBasha <88436007+ChandiniBasha@users.noreply.github.com>
2021-12-07 10:38:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+buttons.forEach(function(button) {
+    
+    button.addEventListener('click', calculate);
+
+});
+
+function calculate(event) {
+
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+
+    if (display.value !== '') {
+
+      display.value = eval(display.value);
+
+    }
+
+  } else if (clickedButtonValue === 'Reset') {
+
+    display.value = '';
+
+  } else {
+
+    display.value += clickedButtonValue;
+  }
+}
+
